4b0d674c47a8b38fa2bf3233f2890965f66eb93b
Derek Shao <shao.derek.now@gmail.com>
2018-10-14 13:49:28 -0400

    Initial Combat engine and default Unit class

---
+++ src/assets/Unit.java
@@ -0,0 +1,26 @@
+package assets;
+
+/**
+ * Attributes shared by both Plant and Zombie
+ * 
+ * @author Open
+ *
+ */
+public class Unit {
+	
+	/**
+	 * I'm leaving this for someone else to do. Below is my proposed interface - derek
+	 * Proposed interface: 
+	 * 
+	 * Attributes:
+	 * 1. private int hp; // hit points of this unit
+	 * 2. private int xcoord; // x coordinate of this unit
+	 * 3. private int ycoord; // y coordinate of this unit
+	 * 4. private int power; // power level - the amount of damage this unit can do per attack
+	 * 
+	 * Methods:
+	 * 1. Getter and Setter for all attributes
+	 * 2. Method to decrease this units' hit points based on the power level of attacker. Returns true if this unit is alive and false if dead
+	 * 
+	 */
+}
--- src/engine/Combat.java
+++ src/engine/Combat.java
@@ -1,11 +1,33 @@
 package engine;
 
+import assets.Unit;
+
 /**
- * Handles Combat Calculations
- * @author david
+ * Handles combat calculations. Acts as a mediator between Plant and Zombie interaction.
+ * 
+ * @author Derek Shao
  *
  */
 public class Combat {
-	private Combat() {} // Singleton
 	
+	/* Singleton */
+	private Combat() {}
+	
+	/**
+	 * 
+	 * @param source
+	 * @param target
+	 * @return
+	 */
+	public static boolean attack(Unit source, Unit target) {
+		
+		/**
+		 * 
+		 * 1. Get source's power level
+		 * 2. Deduct the target's hit points based on source's power level
+		 * 3. Return true if target is dead, false otherwise
+		 * 
+		 */
+		return false;
+	}
 }
