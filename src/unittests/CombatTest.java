51c2c45d65cfc63653c3bb495e76e97c1b3ddada
tanishagarg2 <43581234+tanishagarg2@users.noreply.github.com>
2018-11-15 19:57:41 -0500

    CombatTest and PurseTest

--- src/unittests/CombatTest.java
+++ src/unittests/CombatTest.java
@@ -1,29 +1,37 @@
 package unittests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
+import engine.*;
+import assets.*;
 
 import org.junit.jupiter.api.Test;
 
 public class CombatTest {
-
-	@Test
-	public void testCombatConstructor() {
-		
-	}
 	
 	@Test
 	public void testPlantAttack() {
-		
+		Combat c = new Combat(new Grid[1][1]);
+		Board b = new Board(7, 1);
+		Peashooter p = new Peashooter();
+		Regular_Zombie z = new Regular_Zombie();
+		b.placePlant(p, 0, 1);
+		b.placeZombie(z, 7, 1);
+		int[] expectedCoordinates = {7, 1};
+		int[] killedZombieCoordinates = c.plantAttack(p);
+		assertEquals("Zombie's row = 7 and row =1", expectedCoordinates, killedZombieCoordinates);
 	}
 	
 	@Test
 	public void testZombieAttack() {
-		
+		Combat c = new Combat(new Grid[1][1]);
+		Board b = new Board(7, 1);
+		Peashooter p = new Peashooter();
+		Regular_Zombie z = new Regular_Zombie();
+		b.placePlant(p, 0, 1);
+		b.placeZombie(z, 7, 1);
+		assertTrue("True", c.zombieAttack(z, p));
 	}
 	
-	@Test
-	public void testUnitIsDead() {
-		
-	}
-
 }
--- src/unittests/PurseTest.java
+++ src/unittests/PurseTest.java
@@ -1,14 +1,29 @@
 package unittests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+import engine.*;
 
-class PurseTest {
+public class PurseTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testPurseConstructor() {
+		Purse p = new Purse(25);
+		assertEquals("Points = 25", 25, p.getPoints());
+	}
+	
+	public void testAddPoints() {
+		Purse p = new Purse(25);
+		p.addPoints(25);
+		assertEquals("Points = 50", 50, p.getPoints());
+	}
+	
+	public void testSpendPointsMethods() {
+		Purse p = new Purse(50);
+		assertTrue("True", p.canSpend(25));
+		assertTrue("True", p.spendPoints(25));
 	}
 
 }
