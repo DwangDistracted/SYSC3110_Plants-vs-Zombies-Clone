85adfd153ec9d09122bead1c0efa1f3938f3b308
David Wang <david.lwang.1998@gmail.com>
2018-11-16 16:57:13 -0500

    Game Update for Milestone 2

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -57,34 +57,10 @@ public class Game {
 	}
 	
 	/**
-	 * Processes a Player's Turn
-	 */
-	private void playerTurn() {
-		LOG.info("It is your turn. You have " + userResources.getPoints() + " sunshine.");
-		LOG.prompt(CommandWords.getPrimaryGameCommands());
-		
-		while(!processCommand(Parser.getCommand())) {
-			LOG.prompt(CommandWords.getPrimaryGameCommands());
-		}
-		
-		//plants action
-		List<Plant> plantsInGame = board.getPlantsInGame();
-		
-		for (Plant plant : plantsInGame) {
-			if (!(plant instanceof Flower)) {
-				int [] zombieTargetCoordinates = combat.plantAttack(plant);
-				if (zombieTargetCoordinates != null) {
-					board.removeZombie(zombieTargetCoordinates[0], zombieTargetCoordinates[1]);
-				}
-			}
-		}
-	}
-	
-	/**
 	 * Processes the Zombie's Turn.
 	 * @author David Wang; Modified by Derek Shao
 	 */
-	public void zombieTurn() {
+	private void zombieTurn() {
 		LOG.info("It is the zombie's turn.");
 		
 		//create a new collection to prevent concurrent modification of Board zombies attribute
@@ -151,7 +127,9 @@ public class Game {
 	/**
 	 * Tells Combat Engine to handle attack and damage calculations. Adds Resources to Player Purse. Checks if the pLayer has won
 	 */
-	private void doEndOfTurn() {
+	public void doEndOfTurn() {
+		//do the zombie Turn
+		zombieTurn();
 		
 		//economy calculations
 		userResources.addPoints(levelInfo.getResPerTurn()); //do default sunshine gain
@@ -176,130 +154,6 @@ public class Game {
 			LOG.info("Player was eaten by Zombies");
 		}
 	}
-	
-	
-	//INPUT - MILESTONE 1 ONLY
-
-	 /**
-	  * Displays a help message informing user on the type of zombies they will face within the level,
-	  * the plants that are at that disposal and how to use the command line.
-	  * @author Michael Patsula
-	  */
-	 public void printHelp()
-	 {
-		 Map<ZombieTypes,Integer> allowedZombies = levelInfo.getZombies();
-		 Set<PlantTypes> allowedPlants = levelInfo.getAllowedPlants();
-		 StringBuilder s = new StringBuilder();
-		 
-		 s.append("Help message: to place a unit Type 'Place <unitName> <row #> <column #>. \nNote: row & column numbers start at coordinate 0");
-		 
-		 s.append("\nThe plants commands at your disposal are: ");
-		 for(PlantTypes p : allowedPlants)
-		 {
-			 s.append(p.toString() + " | ");
-		 }
-		 s.append("\nThe zombies that you will face against are: ");
-		 for(ZombieTypes z : allowedZombies.keySet())
-		 {
-			 s.append(z.toString() + " | ");
-		 }
-		 LOG.info(s.toString());
-	 }
-	 
-	/**
-	 * Determines if the user inputed commands is within the grid boundaries.
-	 * @param row - coordinate
-	 * @param column - coordinate
-	 * @return true if the parameters are within the grid boundaries, and false otherwise.
-	 * @author Michael Patsula
-	 */
-	 public boolean inRange(int row, int column)  
-	 {
-		if(row <= levelInfo.getRows() && row >= 0 && column < levelInfo.getColumns() && column >= 0)  // board.getRow and column was subtracted by one due to arrays
-			return true;
-			
-		return false;
-	 }
-
-	/**
-	 * Processes the user input
-	 * @param command - the user input
-	 * @return true if the input was valid, false otherwise
-	 * @author Michael Patsula and refactored by David Wang
-	 */
-	 public boolean processCommand(Command command)
-	 {
-		 if (command == null) { return false; } 
-		 
-		 String commandWord = command.getWord(1);
-
-		 if (commandWord.equalsIgnoreCase(CommandWords.HELP.toString())) {
-			 printHelp();
-			 return false;
-
-		 } else if (commandWord.equalsIgnoreCase(CommandWords.QUIT.toString())) {
-			 finished = true;
-			 LOG.info("User quit the game");
-			 return true;
-
-		 } else if (commandWord.equalsIgnoreCase(CommandWords.PASS.toString())) {
-			 return true;
-
-		 } else if (commandWord.equalsIgnoreCase(CommandWords.PLACE.toString())) {
-			 if (command.isUnknownWord(3) || command.isUnknownWord(4)) // restriction - check if word 3 and 4 is null
-			 {
-				 LOG.warn("Please input your command with coordinates");
-				 return false;
-			 } else { // restriction 2 see if the coordinates are in range of the board
-				 boolean inRange;
-				 try {
-					 inRange = inRange(Integer.valueOf(command.getWord(3)), Integer.valueOf(command.getWord(4))); // check
-					 // valid
-					 // coordinates
-				 } catch (Exception e) {
-					 LOG.error("Exception: " + e.getMessage());
-					 e.printStackTrace();
-					 return false;
-				 }
-
-				 if (!inRange) {
-					 LOG.warn("The inputted coordinates are out of range");
-					 return false;
-				 }
-			 }
-
-			 if (CommandWords.isValidUnit(command.getWord(2))) {
-				 if (levelInfo.getAllowedPlants().contains(PlantTypes.valueOf(command.getWord(2).toUpperCase()))) {
-					 Plant p = PlantTypes.toPlantFromString(command.getWord(2));
-					 if (userResources.canSpend(p.getCost())) {
-						 int plantRow = Integer.valueOf(command.getWord(3));
-						 int plantCol = Integer.valueOf(command.getWord(4));
-						 userResources.spendPoints(p.getCost());
-						 if (!board.placePlant(p, plantRow, plantCol)) {
-							 LOG.info(String.format("Cannot place plant: %s at tile: (%s, %s) because the tile is already occupied",
-									 command.getWord(2), command.getWord(3), command.getWord(4)));
-							 return false;
-						 }
-						 p.setCoordinates(plantRow, plantCol);
-						 LOG.info(command.getWord(2) + " was placed on tile " + command.getWord(3) + ", "
-								 + command.getWord(4));
-						 return true;
-					 } else {
-						 LOG.warn("Cannot Afford Unit");
-						 return false;
-					 }
-				 } else {
-					 LOG.warn("Unit not Available");
-					 return false;
-				 }
-			 } else {
-				 LOG.warn("Not a Valid Unit");
-				 return false;
-			 }
-		 }
-		 LOG.warn("Invalid Command");
-		 return false;
-	 }
 	 
 	 /**
 	  * Get the LevelInfo 
