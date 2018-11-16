9b0c1e5eba3b6ddff9a89a169890c9f651d08261
David Wang <david.lwang.1998@gmail.com>
2018-11-16 17:38:58 -0500

    Added win/loss detection

--- src/ui/GameUI.java
+++ src/ui/GameUI.java
@@ -21,7 +21,7 @@ import assets.Regular_Zombie;
  */
 
 public class GameUI extends JFrame
-{
+{	
 	private static final long serialVersionUID = -717683255015646823L;
 
 	private JPanel gui;
@@ -49,8 +49,6 @@ public class GameUI extends JFrame
     private LevelInfo lvl;
     private Purse userResources;
 	private Board gameBoard;
-
-	private ArrayList<Card> cards;
 
     public GameUI(Game game)
     {
