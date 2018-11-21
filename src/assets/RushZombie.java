c88db31fb9ff1c61504d144f0b5cab020b6c6f6f
David Wang <david.lwang.1998@gmail.com>
2018-11-21 17:09:44 -0500

    rebalanced the rush zombie

--- src/assets/RushZombie.java
+++ src/assets/RushZombie.java
@@ -3,7 +3,7 @@ package assets;
 public class RushZombie extends Zombie {
 	private static final int DEFAULT_SPEED = 2;
 	private static final int DEFAULT_POWER = 3;
-	private static final int DEFAULT_HP = 4;
+	private static final int DEFAULT_HP = 2;
 	private static final ZombieTypes ZOMBIE_TYPE = ZombieTypes.RUSH_ZOMBIE;
 	
 	public RushZombie()	{
