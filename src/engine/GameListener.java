8884cdeed1b38a2ba56c90ad8d87e9171d75f216
David Wang <david.lwang.1998@gmail.com>
2018-11-23 15:25:57 -0500

    GameListener

---
+++ src/engine/GameListener.java
@@ -0,0 +1,13 @@
+package engine;
+
+/**
+ * The Views that listen to the game need to 
+ * @author david
+ *
+ */
+public interface GameListener {
+	public void updateGrid(int x, int y);
+	public void updateAllGrids();
+	public void updatePurse();
+	public void updateTurnNumber();
+}
