b74398c3b6707c43989b7d1bfeb3fcc10224d149
Derek Shao <shao.derek.now@gmail.com>
2018-11-25 10:05:32 -0500

    View tests

---
+++ src/unittests/ViewTest.java
@@ -0,0 +1,106 @@
+package unittests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import assets.PlantTypes;
+import engine.Board;
+import engine.Purse;
+import engine.CommandQueue;
+import engine.Game;
+import levels.LevelInfo;
+import levels.LevelLoader;
+import ui.GameUI;
+import ui.GridUI;
+
+public class ViewTest {
+	
+	private static Game game;
+	private static Board board;
+	private static Purse purse;
+	private static CommandQueue commandQueue;
+	private static LevelInfo lvl;
+	private static GameUI gameUI;
+	private static GridUI[][] boardTiles;
+	
+	@BeforeAll
+	public static void loadLevel() {
+		LevelLoader.init();
+		lvl = LevelLoader.getLevel(1);
+	}
+	
+	@BeforeEach
+	public void setUp() {
+		GameUI.setTestMode();
+		game = new Game(lvl);
+		board = game.getBoard();
+		purse = game.getPurse();
+		commandQueue = game.getCommandQueue();
+		gameUI = new GameUI(game);
+		boardTiles = gameUI.getBoardTiles();
+	}
+	
+	@Test
+	public void testPlacingPlantUpdatesView() {
+		try {
+			game.placePlant(PlantTypes.SUNFLOWER, 0, 0);
+		}
+		catch (Exception e) {
+			e.addSuppressed(new ClassCastException());
+		}
+		GridUI placedLocation = boardTiles[0][0];
+		assertNotNull(placedLocation.getPlantPanel());
+		JPanel plantPanel = placedLocation.getPlantPanel();
+		JLabel plantName = (JLabel) plantPanel.getComponent(0);
+		assertNotNull(plantName);
+		assertEquals(plantName.getText(), "Sunflower");
+	}
+	
+	@Test 
+	public void testRemovingPlantUpdatesView() {
+		testPlacingPlantUpdatesView();
+		game.removePlant(0, 0);
+		GridUI removedLocation = boardTiles[0][0];
+		JPanel plantPanel = removedLocation.getPlantPanel();
+		assertTrue(plantPanel.getComponents().length == 0);
+	}
+	
+	@Test
+	public void testUpdateTurnLabel() {
+		game.doEndOfTurn();
+		JLabel turnMessage = gameUI.getTurnLabel();
+		assertEquals(turnMessage.getText(), "<html><b>Turns: </b>1</html>");
+	}
+	
+	@Test
+	public void testUpdatePointsLabel() {
+		game.doEndOfTurn();
+		JLabel pointsMessage = gameUI.getPointsLabel();
+		assertEquals(pointsMessage.getText(), "<html><b>Points: </b>" + Integer.toString(purse.getPoints()) + "</html>");
+		
+		game.placePlant(PlantTypes.SUNFLOWER, 0, 0);
+		pointsMessage = gameUI.getPointsLabel();
+		assertEquals(pointsMessage.getText(), "<html><b>Points: </b>" + Integer.toString(purse.getPoints()) + "</html>");
+	}
+	
+	@Test
+	public void testUpdatePointsDoesNotDisplayNegative() {
+		Purse emptyPurse = new Purse(0);
+		purse.setPoints(emptyPurse);
+		gameUI.updatePurse();
+		
+		game.placePlant(PlantTypes.SUNFLOWER, 0, 0);
+		JLabel pointsMessage = gameUI.getPointsLabel();
+		assertEquals(pointsMessage.getText(), "<html><b>Points: </b>" + Integer.toString(purse.getPoints()) + "</html>");
+	}
+}
