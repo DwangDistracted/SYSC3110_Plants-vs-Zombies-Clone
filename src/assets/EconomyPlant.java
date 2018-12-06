e4a300f0de86695c0616c19fb5c128887e42e76c
Michael Patsula <michael.patsula@gmail.com>
2018-12-06 10:43:34 -0500

    Overidding points

--- src/assets/EconomyPlant.java
+++ src/assets/EconomyPlant.java
@@ -1,16 +1,25 @@
 package assets;
-
+/**
+ * Abstract Class for plants that generate points.  
+ * Every class that extends EconomyPlant should produce sunshine points after every turn.
+ * @author Michael Patsula
+ *
+ */
 public abstract class EconomyPlant extends Plant {
 
-	public static final int points = 10;
+	private int POINTS = 10;
 	
-	public EconomyPlant(int hp, int pwr, int cost) {
+	public EconomyPlant(int hp, int pwr, int cost, int points) {
 		super(hp, pwr, cost);
+		this.POINTS = points;
 	}
 	
-	public int getPoints()
-	{
-		return points;
+	/**
+	 * Retrieves the amount of points this plant generates per turn
+	 * @return the number of points the plant produces
+	 */
+	public int getPoints(){
+		return POINTS;
 	}
 
 }
--- src/assets/Twin_Flower.java
+++ src/assets/Twin_Flower.java
@@ -13,14 +13,12 @@ public class Twin_Flower extends EconomyPlant implements Serializable {
 	private static final int DEFAULT_HP = HEALTH_LOW;
 	private static final int DEFAULT_POWER = ATTACK_NONE;
 	private static final int COST = 50;
-	
 	//The Amount of Points that are added per turn per sunflower
-	private static final int POINTS = 20; //points that a sunflower provides a player to buy other plants
-
+	private static final int POINTS = 20; 
 	private static final PlantTypes PLANT_TYPE = PlantTypes.TWIN_FLOWER;
 	
 	public Twin_Flower()	{
-		super(DEFAULT_HP, DEFAULT_POWER, COST);
+		super(DEFAULT_HP, DEFAULT_POWER, COST, POINTS);
 	}
 	
 	/**
